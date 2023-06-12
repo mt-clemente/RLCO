9dffb586afdd9228d684d138bc6505f0560f4fbf
mt-clemente <mateo.clem@gmail.com>
2023-06-11 23:18:19 -0400

    Getting there?

--- buffer.py
+++ buffer.py
@@ -6,14 +6,16 @@ import torch
 class Buffer():
 
     #TODO: custom state init, if some segments of the solution are known
+    # CHECK WHICH ELEMNTS HAVE TO BE BUFFERED AND WHICH DO NOT ---> HORIZON VS SEQUENCE
 
-    def __init__(self,cfg,max_ep_len,device='cpu',init_state=None) -> None:
+    def __init__(self,cfg,num_instances,max_ep_len,device='cpu',init_state=None) -> None:
         self.unit = cfg['unit']
         self.capacity = cfg['training']['horizon']
         self.device = device
         self.dim_token=cfg['dim_token']
         self.init_state = init_state
         self.max_ep_len = max_ep_len
+        self.num_instances = num_instances
 
     def push(
             self,
@@ -21,12 +23,14 @@ class Buffer():
             action,
             policy,
             reward,
+            mask,
             ep_step,
             final
             ):
 
         self.state_buf[self.ptr+1] = state
         self.policy_buf[self.ptr] = policy
+        self.mask_buf[self.ptr] = mask
         self.rew_buf[self.ptr] = reward
         self.timestep_buf[self.ptr] = ep_step
         self.final_buf[self.ptr] = final
@@ -35,17 +39,17 @@ class Buffer():
         self.ptr += 1
 
     def reset(self):
-        self.state_buf = torch.zeros((self.capacity,self.max_ep_len,self.dim_token),device=self.device,dtype=self.unit)
+        self.state_buf = torch.zeros((self.num_instances,self.capacity,self.max_ep_len,self.dim_token),device=self.device,dtype=self.unit)
         if not self.init_state is None:
             self.state_buf[0] = self.init_state
-        self.act_buf = torch.empty((self.capacity),dtype=int,device=self.device)
-        self.rtg_buf = torch.zeros((self.capacity),device=self.device,dtype=self.unit)
-        self.policy_buf = torch.empty((self.capacity),device=self.device,dtype=self.unit)
-        self.mask_buf = torch.empty((self.capacity,self.capacity),dtype=bool,device=self.device)
-        self.rew_buf = torch.empty((self.capacity),device=self.device,dtype=self.unit)
-        self.final_buf = torch.empty((self.capacity),dtype=int,device=self.device)
-        self.adv_buf = torch.zeros((self.capacity),device=self.device,dtype=self.unit)
-        self.timestep_buf = torch.empty((self.capacity),device=self.device,dtype=int)
+        self.act_buf = torch.empty((self.num_instances,self.capacity),dtype=int,device=self.device)
+        self.rtg_buf = torch.zeros((self.num_instances,self.capacity),device=self.device,dtype=self.unit)
+        self.policy_buf = torch.empty((self.num_instances,self.capacity),device=self.device,dtype=self.unit)
+        self.mask_buf = torch.empty((self.num_instances,self.capacity,self.max_ep_len4),dtype=bool,device=self.device)
+        self.rew_buf = torch.empty((self.num_instances,self.capacity),device=self.device,dtype=self.unit)
+        self.final_buf = torch.empty((self.num_instances,self.capacity),dtype=int,device=self.device)
+        self.adv_buf = torch.zeros((self.num_instances,self.capacity),device=self.device,dtype=self.unit)
+        self.timestep_buf = torch.empty((self.num_instances,self.capacity),device=self.device,dtype=int)
         self.ptr = 0
 
 
