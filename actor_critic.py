Segment masking needs fixing

actor_critic.py
@@ -205,7 +205,7 @@ class Actor(nn.Module):
                 tgt_key_padding_mask=tgt_key_padding_mask
             )
 
-            policy_logits = self.actor_head(policy_tokens[torch.arange(batch_size,device=policy_tokens.device),timesteps].reshape(batch_size,self.dim_embed))
+            policy_logits = self.actor_head(policy_tokens[torch.arange(batch_size,device=policy_tokens.device),timesteps.squeeze()].reshape(batch_size,self.dim_embed))
             policy_pred = self.policy_head(policy_logits,valid_action_mask)
 
         return policy_pred
@@ -257,7 +257,8 @@ class Critic(nn.Module):
             tgt_key_padding_mask=tgt_key_padding_mask
         )
 
-        value_pred = self.critic_head(tgt_tokens[torch.arange(batch_size,device=tgt_tokens.device),timesteps.squeeze()+1].reshape(batch_size,self.dim_embed))
+        tgt_tokens = tgt_tokens[torch.arange(batch_size,device=tgt_tokens.device),timesteps.squeeze()+1].reshape(batch_size,self.dim_embed)
+        value_pred = self.critic_head(tgt_tokens)
 
         return value_pred
 

cop_class.py
@@ -133,7 +133,8 @@ class COProblem(ABC):
             return torch.logical_not(torch.logical_and(self.valid_action_mask(states,segments),used_mask))
 
 
-
+    def display_solution(self,solution,file):
+        raise NotImplementedError
 
 
 # class InstanceBatchManager():

layers.py
@@ -151,7 +151,10 @@ class MaskedStableSoftmax(nn.Module):
         self.eps = eps
 
     def forward(self, logits,mask):
-        if mask.count_nonzero() == 0:
+
+        if torch.logical_not(mask).count_nonzero() == 0:
             raise Exception("No playable tile")
         logits = logits - logits.max(dim=-1, keepdim=True).values
-        return torch.softmax(logits - self.eps* torch.logical_not(mask),-1) 
+        print(torch.logical_not(mask).count_nonzero(dim=-1))
+        print(torch.argmax(torch.softmax(logits - self.eps*mask,-1),-1))
+        return torch.softmax(logits - self.eps*mask,-1) 

main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import torch
+import wandb
 from solver import RLCOSolver
 import argparse
 from problems.eternity_cop import Eternity
@@ -31,7 +32,7 @@ TODO:
 
 """
 torch.cuda.is_available = lambda : False
-
+torch.autograd.set_detect_anomaly(True)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--cfg', dest='cfg',type=str,default='config.yml')
@@ -41,10 +42,20 @@ args = parser.parse_args()
 
 problem = Eternity
 
+
 trainer = RLCOSolver(
     config_path=args.cfg,
     instances_path=Path(args.files),
     problem=Eternity,
 )
 
+wandb.init(
+    project='EteRLnity',
+    entity='mateo-clemente',
+    group='NewTests',
+    config=trainer.cfg
+)
+
 trainer.train()
+
+wandb.finish()

ppo.py
@@ -8,7 +8,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import wandb
 from torch.utils.data import TensorDataset,DataLoader
-from torchviz import make_dot
 
 from actor_critic import ActorCritic
 from buffer import Buffer
@@ -101,13 +100,13 @@ class PPOAgent(nn.Module):
         #     pass
 
         # FIXME: Timesteps
-
         # State dims are [instance,step,state,token]
         states = torch.cat((buf.state_buf,buf.horzion_states.unsqueeze(1)),dim=1).transpose(0,1) # --> [step, instance, state, token]
         timesteps = torch.cat((buf.timestep_buf,buf.horzion_timesteps.unsqueeze(1)),dim=1).transpose(0,1).unsqueeze(-1)
         rewards = buf.rew_buf
         finals = buf.final_buf
-        values = torch.empty((states.size(1),states.size(0)))
+
+        value_steps = []
         for i, (state_step, timestep_step) in enumerate(zip(states, timesteps)):
             
             embedded_state_step,embedded_segment_step = self.pb.tokenize(state_step,segments)
@@ -125,11 +124,12 @@ class PPOAgent(nn.Module):
                 src_key_padding_mask=src_key_padding_masks
             )
 
-            values[:,i] = value_step.squeeze()
+            value_steps.append(value_step.squeeze())
 
+        values = torch.stack(value_steps, dim=1)
         next_values = values[:,1:].detach()
         values = values[:,:-1]
-        buf.value_buf[:,:] = values
+
 
         # FIXME: Check if there is a problem with the new shapes
 
@@ -152,7 +152,7 @@ class PPOAgent(nn.Module):
         advantages = advantages
         returns_to_go = returns_to_go
 
-        return advantages, returns_to_go
+        return advantages, returns_to_go,values
 
     def rollout(self,manager:TrainingManager):
 
@@ -168,22 +168,26 @@ class PPOAgent(nn.Module):
             steps = manager.get_ep_step()
             action_masks = self.pb.valid_action_mask_(states,segments,manager.masks)
 
-            policy = self.get_policy(
-                states=states,
-                segments=segments,
-                timesteps=steps,
-                valid_action_mask=action_masks,
-                src_key_padding_masks=manager.src_key_padding_masks,
-                tgt_mask=manager.causal_mask
-            )
+            with torch.no_grad():
+                policy = self.get_policy(
+                    states=states,
+                    segments=segments,
+                    timesteps=steps,
+                    valid_action_mask=action_masks,
+                    src_key_padding_masks=manager.src_key_padding_masks,
+                    tgt_mask=manager.causal_mask
+                )
 
             new_states, rewards, probs, actions = self.increment_states(
                 states=states,
                 segments=segments,
                 policies=policy,
                 steps=steps,
+                sizes=manager.sizes,
                 
             )
+
+            finals = manager.get_finals()
 
             self.buf.push(
                 state=states,
@@ -192,19 +196,22 @@ class PPOAgent(nn.Module):
                 mask=action_masks,
                 reward=rewards,
                 ep_step=steps,
-                final=manager.get_finals()
+                final=finals
             )
         
             states = new_states
-
+            states[finals,:,:] = manager.init_states[finals,:,:]
             # update finals / masks based on actions taken
             manager.step(actions)
+
         # inject the states at the end of the horizon, needed to calculate advantages
         manager.finalize_rollout(states)
+
+        self.pb.display_solution(states[0],'temp.png',manager.sizes[0])
         self.buf.horzion_timesteps = steps + 1 % manager.instance_lengths
 
         
-    def increment_states(self,states,segments:torch.Tensor,policies:torch.Tensor,steps):
+    def increment_states(self,states,segments:torch.Tensor,policies:torch.Tensor,steps,sizes):
         """
         Adds the tokens selected by the policies to the current states.
         Returns new states, associated rewards, probabilities for each action w.r.t the current
@@ -214,15 +221,15 @@ class PPOAgent(nn.Module):
         types
         """
 
+        self.pb.display_solution(states[0],'tempm1.png',sizes[0])
         actions = torch.multinomial(policies,1).squeeze()
-
         added_tokens = segments[torch.arange(segments.size(0),device=segments.device),actions]
-
         # FIXME: Remove that
         if 0 in added_tokens.sum(-1).squeeze():
-            raise ValueError("Null token chosen: in the following segments : ",added_tokens)
+            raise ValueError("Null token chosen: in the following segments : ",added_tokens,actions,sizes)
         
-        new_states, rewards = self.pb.act(states,added_tokens,steps)
+        new_states, rewards = self.pb.act(states,added_tokens,steps,sizes)
+        print(added_tokens[0])
         return new_states, rewards, policies[torch.arange(policies.size(0)),actions], actions
 
 
@@ -239,7 +246,7 @@ class PPOAgent(nn.Module):
 
         t0 = datetime.now()
 
-        advantages, returns = self.compute_gae_rtg(
+        advantages, returns,values = self.compute_gae_rtg(
             buf=self.buf,
             gamma=self.train_cfg['gamma'],
             gae_lambda=self.train_cfg['gae_lambda'],
@@ -250,7 +257,7 @@ class PPOAgent(nn.Module):
         dataset = TensorDataset(
             rearrange(self.buf.state_buf,'i s p t -> (i s) p t'),
             rearrange(self.buf.act_buf,'i s -> (i s)'),
-            rearrange(self.buf.value_buf,'i s -> (i s)'),
+            rearrange(values,'i s -> (i s)'),
             rearrange(self.buf.mask_buf,'i s m -> (i s) m'),
             rearrange(self.buf.policy_buf,'i s -> (i s)'),
             rearrange(self.buf.timestep_buf,'i s -> (i s)'),
@@ -259,6 +266,18 @@ class PPOAgent(nn.Module):
             torch.arange(manager.num_instances,device=manager.device).repeat_interleave(self.buf.act_buf.size(1))
         )
 
+
+        # dataset = TensorDataset(
+        #     self.buf.state_buf[0],
+        #     self.buf.act_buf[0],
+        #     values[0],
+        #     self.buf.mask_buf[0],
+        #     self.buf.policy_buf[0],
+        #     self.buf.timestep_buf[0],
+        #     advantages[0],
+        #     returns[0],
+        #     torch.arange(1,device=manager.device).repeat_interleave(self.buf.act_buf.size(1))
+        # )
 
         loader = DataLoader(dataset, batch_size=self.train_cfg['minibatch_size'], shuffle=True, drop_last=False)
 
@@ -288,6 +307,14 @@ class PPOAgent(nn.Module):
                     embedded_segments,
                     batch_timesteps.unsqueeze(-1),
                 )
+
+                batch_values = self.model.critic(
+                    src_inputs=src_inputs,
+                    tgt_inputs=tgt_inputs,
+                    timesteps=batch_timesteps,
+                    tgt_key_padding_mask=tgt_key_padding_mask,
+                    src_key_padding_mask=manager.src_key_padding_masks[instance_ids]
+                )
                 
                 batch_policy = self.model.actor.forward(
                     src_inputs=src_inputs,
@@ -299,7 +326,6 @@ class PPOAgent(nn.Module):
 
                 )
 
-                print(batch_states.size())
                 batch_advantages = (batch_advantages - batch_advantages.mean()) / (batch_advantages.std()+1e-4)
                 batch_returns_norm = (batch_returns - batch_returns.mean()) / (batch_returns.std()+1e-4)
 
@@ -316,8 +342,6 @@ class PPOAgent(nn.Module):
                 entropy = -(batch_policy[batch_policy != 0] * torch.log(batch_policy[batch_policy != 0])).sum(dim=-1).mean()
                 entropy_loss = -self.entropy_weight * entropy
                 # Compute total loss and update parameters
-
-
 
                 if self.train_cfg['separate_value_training']:
                     pol_loss = policy_loss + entropy_loss
@@ -337,11 +361,10 @@ class PPOAgent(nn.Module):
 
                 else:
                     
-                    print("---------->BATCH")
-                    loss = policy_loss + value_loss + entropy_loss.detach()
+                    loss = policy_loss + value_loss + entropy_loss
                     self.model.optimizer.zero_grad()
                     loss.backward()
-                  
+
                     if self.grad_clip:
                         torch.nn.utils.clip_grad_norm_(self.model.parameters(),self.grad_clip)
                   
@@ -368,14 +391,16 @@ class PPOAgent(nn.Module):
 
         print(datetime.now()-t0)
         wandb.log({
-            "Current learning rate":self.scheduler.get_last_lr()[0],
+            # "Current learning rate":self.scheduler.get_last_lr()[0],
             "Value loss":value_loss,
             "Entropy loss":entropy_loss,
             "Policy loss":policy_loss,
             "Value repartition":batch_values.squeeze(-1).detach(),
-            "Total KL div": (batch_old_policies * (torch.log(batch_old_policies + 1e-5) - torch.log(batch_policy + 1e-5))).sum(dim=-1).mean()
+            # "Total KL div": (batch_old_policies * (torch.log(batch_old_policies + 1e-5) - torch.log(batch_policy + 1e-5))).sum(dim=-1).mean()
             })
 
+
+        self.buf.reset()
 
         # for worker in self.workers:
         #     worker.load_state_dict(self.model.state_dict())

problems/eternity/utils.py
@@ -18,14 +18,23 @@ def parse_arguments():
 
     return parser.parse_args()
 
-def toseq(state:torch.Tensor,remove_padding=True):
-    if remove_padding:
-        return rearrange(state[1:-1,1:-1],'h w d -> (h w) d')
-    
-    return rearrange(state,'h w d -> (h w) d')
+def toseq(state:torch.Tensor,size=None,remove_borders=True):
+    if remove_borders:
+        ret = rearrange(state[1:-1,1:-1],'h w d -> (h w) d')
 
-def fromseq(state:torch.Tensor):
-    return rearrange(state,'(h w) d -> h w d',h=int(state.size(0)**0.5))
+    else:
+        ret = rearrange(state,'h w d -> (h w) d')
+        
+    if size is None:
+        return ret
+    
+    return torch.nn.functional.pad(ret,(0,0,0,size-len(ret)),'constant',0)
+
+def fromseq(state:torch.Tensor,size,pad=True):
+    ret = rearrange(state[:size],'(h w) d -> h w d',h=int(size**0.5))
+    if pad:
+        return torch.nn.functional.pad(ret,(0,0,1,1,1,1),'constant',0)
+    return ret
 
 
 def initialize_sol(pz:EternityPuzzle, device):

problems/eternity_cop.py
@@ -24,29 +24,31 @@ class Eternity(COProblem):
             device=device
             )
         
-        
+    
 
     def load_instance(self, path: Path) -> COPInstance:
         pz = EternityPuzzle(path)
         state, tiles, _, n_tiles = initialize_sol(pz,self.device)
         self.num_segments = n_tiles
+        self.pz = pz
         return COPInstance(toseq(state).int(),tiles.int(),size = n_tiles,num_segments=n_tiles-1)
     
 
     # TODO: Fix kwargs
-    def act(self, states:torch.Tensor, segments:torch.Tensor, steps:torch.IntTensor, **kwargs) -> tuple:
+    def act(self, states:torch.Tensor, segments:torch.Tensor, steps:torch.IntTensor,sizes, **kwargs) -> tuple:
 
         rewards = torch.zeros(len(states),device=states.device)
         new_states = torch.empty_like(states)
 
         # TODO FIX SIZE
-        for i, (state, segment, step) in enumerate(zip(
+        for i, (state, segment, step,size) in enumerate(zip(
             states,
             segments,
-            steps
+            steps,
+            sizes
             )):
 
-            n_state, reward = self.place_tile(state,segment,step)
+            n_state, reward = self.place_tile(state,segment,step,size)
 
             new_states[i] = n_state
             rewards[i] = reward
@@ -72,7 +74,7 @@ class Eternity(COProblem):
 
 
 
-    def place_tile(self,state:torch.Tensor,tile:torch.Tensor,ep_step:int,step_offset:int=0):
+    def place_tile(self,state:torch.Tensor,tile:torch.Tensor,ep_step:int,size,step_offset:int=1):
         """
         If you start with a prefilled board with k pieces, you need to place tiles at spot
         k + 1, hence the need for a step offset.
@@ -86,19 +88,18 @@ class Eternity(COProblem):
         best_connect = -1
         best_state = None
         best_reward = None
-        state_ = fromseq(state)
-        size = state.size(0)
+        max_size = state.size(0)
+        state_ = fromseq(state,size)
+        side_size = int(size**0.5)
         for _ in range(4):
             tile = tile.roll(self.color_embedding_size,-1)
-            state_[step // size + 1, step % size + 1,:] = tile
-            conflicts, connect, reward = self.filling_connections(state_,size,step)
-
+            state_[step // side_size+1, step % side_size+1,:] = tile
+            conflicts, connect, reward = self.filling_connections(state_,side_size,step)
             if connect > best_connect:
-                best_state=toseq(state_,remove_padding=False)
+                best_state=toseq(state_,max_size,remove_borders=True)
                 best_connect = connect
                 best_conflict = conflicts
                 best_reward = reward
-
 
         return best_state, best_conflict
 
@@ -111,11 +112,10 @@ class Eternity(COProblem):
         i = step // bsize + 1
         j = step % bsize + 1
         state = torch.nn.functional.pad(state,(0,0,1,1,1,1),"constant",0)
-        west_tile_color = state[i,j-1,3*self.color_embedding_size:4*self.color_embedding_size]
-        south_tile_color = state[i-1,j,:self.color_embedding_size]
-
-        west_border_color = state[i,j,1*self.color_embedding_size:2*self.color_embedding_size]
-        south_border_color = state[i,j,2*self.color_embedding_size:3*self.color_embedding_size]
+        west_tile_color = state[i,j-1,3:4]
+        south_tile_color = state[i-1,j,:1]
+        west_border_color = state[i,j,1:2]
+        south_border_color = state[i,j,2:3]
 
         sides = 0
         connections = 0
@@ -161,3 +161,8 @@ class Eternity(COProblem):
         
         return sides - connections, connections, reward
 
+
+
+    def display_solution(self,state,file,size):
+        self.pz.board_size=int(size**0.5)
+        self.pz.display_solution(state.tolist(),file)

utils.py
@@ -109,6 +109,8 @@ class TrainingManager():
 
         self.curr_step += 1
         self.masks[torch.arange(self.masks.size(0)),actions] = False
+        print(actions,self.masks)
+        print('*********')
         # New episode
         finals = (self.curr_step % self.instance_lengths) == 0
         self.states[finals] = self.init_states[finals]
@@ -126,7 +128,7 @@ class TrainingManager():
     def init_masks(self):
         
         # pad to get all max length sequences
-        src_key_padding_mask = torch.arange(self.max_num_segments, device=self.device).expand((self.num_instances,-1)) >= self.instance_num_segments.unsqueeze(-1)
+        src_key_padding_mask = torch.arange(self.max_num_segments, device=self.device).expand((self.num_instances,-1)) < self.instance_num_segments.unsqueeze(-1)
         
         return src_key_padding_mask
     
@@ -149,7 +151,7 @@ class TrainingManager():
         return self.curr_step % self.instance_lengths
 
     def get_finals(self):
-        return self.curr_step % self.instance_lengths == 0
+        return (self.curr_step+1) % self.instance_lengths == 0
 
     def batch_attributes(self):
         return (